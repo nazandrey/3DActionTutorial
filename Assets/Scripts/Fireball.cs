34fd1874daefc32a9d3a5c6950317c0a1a8739cb
nazandrey <nazandrbox@yandex.ru>
2017-06-27 21:11:51 +0300

    Close #10 Fireball damages player

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Fireball : MonoBehaviour {
 	public float lifetime = 2.0f;
 	public float speed = 3.0f;
+	public float damage = 1.0f;
 
 	void Start () {
 		Destroy (gameObject, lifetime);
@@ -16,6 +17,10 @@ public class Fireball : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
 		if (other.transform.name == "Player") {
+			Health playerHealth = other.GetComponent<Health> ();
+			if (playerHealth != null) {
+				playerHealth.Hurt (damage);
+			}
 			Debug.Log ("Player hit");
 		}
 		Destroy (gameObject);
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour {
-	public float health = 5f;
+	public float health = 5.0f;
+
+	public void Hurt(float damage){
+		health -= damage;
+	}
 }
